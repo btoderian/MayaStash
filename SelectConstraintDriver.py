79e78d2a970c366790b7f32fb625806f38c55bf7
Blaine Toderian <46722757+btoderian@users.noreply.github.com>
2024-11-19 12:44:08 -0800

    Create SelectConstraintDriver.py
    To find driver object in constraint relationship

---
+++ SelectConstraintDriver.py
@@ -0,0 +1,38 @@
+#Find the object driving constraint relationship.  
+
+import maya.cmds as cmds
+
+def selectConstraintDriver():
+   conToNodes = []
+   selObj = cmds.ls(sl=True)
+   for obj in selObj:
+       if "ConTo" in obj:
+           conToNodes.append(obj)
+   sourceObjects = []
+   constraints = cmds.listRelatives(type="constraint")
+   if constraints:
+       for constraint in constraints:
+           sourceObjects.extend(
+               [
+                   sourceObject
+                   for sourceObject in cmds.listConnections(
+                       constraint + ".target[*].targetParentMatrix", s=1
+                   )
+                   if (cmds.objExists(sourceObject) and sourceObject not in sourceObjects)
+               ]
+           )
+   for node in conToNodes:
+       constraints = cmds.listRelatives(cmds.listRelatives(node, parent=True), type="constraint")
+       if constraints:
+           for constraint in constraints:
+               sourceObjects.extend(
+                   [
+                       sourceObject
+                       for sourceObject in cmds.listConnections(
+                           constraint + ".target[*].targetParentMatrix", s=1
+                       )
+                       if (cmds.objExists(sourceObject) and sourceObject not in sourceObjects)
+                   ]
+               )
+   if sourceObjects:
+       cmds.select(sourceObjects)
